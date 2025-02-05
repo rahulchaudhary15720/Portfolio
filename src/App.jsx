9a7617526f2a65c8a6df38b0300446b49dc393ae
rahulchaudhary1572002@gmail.com <rahulchaudhary1572002@gmail.com>
2025-02-06 00:38:23 +0530

    Portfolio Updated

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "../src/components/navbar/Navbar";
+import Home from "../src/pages/home/Home";
+import Experience from "../src/pages/experience/Experience";
+import Projects from "../src/pages/projects/Projects";
+import Contact from "../src/pages/contact/Contact";
+import TopProject from "./components/TopProject/TopProject";
+import Technology from "./components/technology/Technology";
+import Faq from "./components/FAQ/Faq";
+import './App.css';
+import Footer from "./components/footer/Footer";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <div className="m">
+          <Navbar />
+          <main className="route">
+            <Routes>
+              {/* Home route should include all components you want on the home page */}
+              <Route path="/" element={
+                <>
+                  <Home />
+                  <Experience />
+                  <TopProject />
+                  <Technology />
+                  <Faq />
+                </>
+              } />
+              
+              {/* Other routes will only show their specific components */}
+              <Route path="/experience" element={<Experience />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </main>
+          <Footer/>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
